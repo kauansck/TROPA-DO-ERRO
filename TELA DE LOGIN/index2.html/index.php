859bbd077cbf5b74e9aff5cfd382c3f20419bd80
kauan <107212128+kauansck@users.noreply.github.com>
2023-11-29 08:56:05 -0300

    Add files via upload

---
+++ TELA DE LOGIN/index2.html/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <h1>Bem-vindo à minha página</h1>
+    <p>Este é um parágrafo de teste.</p>
+    <div class="container">
+        <h2>Título do container</h2>
+        <p>Conteúdo do container.</p>
+    </div>
+</body>
+</html>
